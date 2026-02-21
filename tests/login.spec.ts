ba072c5363de78ac7860adb8668df4e8b97245d5
Naattyy <natalia1suhajdova@gmail.com>
2026-02-21 14:18:53 +0100

    Doplnenie negatívnych testov

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,17 +1,28 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
+import test, { expect } from '../fixtures/basePages';
 
-test('Successful login', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.gotoLoginPage();
+
+test.describe('Login', () => {
+    test.beforeEach(async ({ loginPage }) => {
+        await loginPage.gotoLoginPage();
+    })
+
+    test('Successful login', async ({ page, loginPage }) => {
     await loginPage.login();
-    await expect(page).toHaveURL('https://dockerdev.prosoft.sk:8083/index.html#/rail/pass');
-});
+    await expect(page).toHaveURL('https://cipkartadmin-dev.kube8s.prosoft.sk/index.html#/rail/pass');
+    });
 
-test.only ('Cannot login with valid username and invalid password', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.gotoLoginPage();
+    test('Cannot login with valid username and invalid password', async ({ page, loginPage }) => {
     await loginPage.enterValidUsername();
     await loginPage.enterInValidPassword();
-    //await expect(loginPage.invalidCredentialsErrorMessage).toBeVisible();
-});
+    await loginPage.clickLoginButton();
+    await expect(loginPage.invalidCredentialsErrorMessage).toBeVisible();
+    });
+
+    test('Cannot login with invalid username and valid password', async ({ page, loginPage }) => {
+    await loginPage.enterInValidUsername();
+    await loginPage.enterValidPassword();
+    await loginPage.clickLoginButton();
+    await expect(loginPage.invalidCredentialsErrorMessage).toBeVisible();
+    });
+
+})
