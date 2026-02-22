5b6b47483c14c58c4d3975584af1fd3054f4ab7e
Naattyy <natalia1suhajdova@gmail.com>
2026-02-22 14:24:15 +0100

    edit

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect } from '../fixtures/basePages';
 
-test('Verify home title', async ({ page, loginPage, homePage }) => {
+test('TC_02_Verify home title', async ({ page, loginPage, homePage }) => {
     await loginPage.gotoLoginPage();
     await loginPage.login();
     await expect(homePage.title).toBeVisible();
