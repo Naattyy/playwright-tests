31ac7b476e1b54a9ad3a57b7e8addc7e8fdcb8a4
Naattyy <natalia1suhajdova@gmail.com>
2026-02-22 14:25:11 +0100

    edit

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,19 +6,19 @@ test.describe('Login', () => {
         await loginPage.gotoLoginPage();
     })
 
-    test('Successful login', async ({ page, loginPage }) => {
+    test('TC_01_Successful login', async ({ page, loginPage }) => {
     await loginPage.login();
     await expect(page).toHaveURL('https://cipkartadmin-dev.kube8s.prosoft.sk/index.html#/rail/pass');
     });
 
-    test('Cannot login with valid username and invalid password', async ({ page, loginPage }) => {
+    test('TC_01.1_Cannot login with valid username and invalid password', async ({ page, loginPage }) => {
     await loginPage.enterValidUsername();
     await loginPage.enterInValidPassword();
     await loginPage.clickLoginButton();
     await expect(loginPage.invalidCredentialsErrorMessage).toBeVisible();
     });
 
-    test('Cannot login with invalid username and valid password', async ({ page, loginPage }) => {
+    test('TC_01.2_Cannot login with invalid username and valid password', async ({ page, loginPage }) => {
     await loginPage.enterInValidUsername();
     await loginPage.enterValidPassword();
     await loginPage.clickLoginButton();
